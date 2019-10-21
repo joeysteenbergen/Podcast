73e41b5b2cdf25c932828dd3ac74ea3bea9545f7
Joey Steenbergen <joeysteenbergen@hotmail.com>
2019-10-21 09:19:54 +0200

    Login layout

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -9,6 +10,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PodcastDAL;
 
 namespace Podcast
 {
@@ -30,7 +32,9 @@ namespace Podcast
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+            services.AddTransient(_ => new DatabaseConnection(Configuration.GetConnectionString("DefaultConnection")));
 
+            services.AddSession();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
@@ -57,7 +61,7 @@ namespace Podcast
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,13 @@
-﻿
+﻿$("#changeLogin").on("click", function () {
+    document.getElementById("Login").style.display = "block";
+    document.getElementById("Register").style.display = "none";
+    document.getElementById("changeLogin").style.backgroundColor = "#9E9E9E";
+    document.getElementById("changeRegister").style.backgroundColor = "#757575";
+});
+
+$("#changeRegister").on("click", function () {
+    document.getElementById("Register").style.display = "block";
+    document.getElementById("Login").style.display = "none";
+    document.getElementById("changeRegister").style.backgroundColor = "#9E9E9E";
+    document.getElementById("changeLogin").style.backgroundColor = "#757575";
+});
